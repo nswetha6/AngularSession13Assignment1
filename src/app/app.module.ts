1c69e38e3637b4b08da3d0eb4ec5fc429b9305c6
swetnaga <swetnaga@SWETNAGA-IN.in.oracle.com>
2018-06-16 18:36:03 +0530

    commit for session13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule,Injectable } from '@angular/core';
+import {FormsModule,FormGroup,ReactiveFormsModule} from '@angular/forms';
+import {Route,Routes,RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { StudentRouteComponent } from './student-route/student-route.component';
+import { DepartmentService } from './student-route/department.service';
+import { StudentRouteService } from './student-route/studentroute.service';
+import { StudentlistComponent } from './studentlist/studentlist.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StudentinfoComponent } from './studentinfo/studentinfo.component';
+import { StudentCanDeactivateService } from './student-route/studentCanDeactivate.service';
+import { NamesearchPipe } from './pipes/namesearch.pipe';
 
+
+const appRoutes: Routes =[
+  {path:'AddStudent', 
+    component: StudentRouteComponent,
+    //canDeactivate:[StudentCanDeactivateService] 
+  },
+  {path:'StudentsList', component: StudentlistComponent },
+  {path:'StudentInfo', component: StudentinfoComponent },
+  //{path:'StudentsList/:FirstName', component: StudentinfoComponent },
+   {path:'StudentInfo/:id', component: StudentinfoComponent },
+  {path:'', redirectTo:'/AddStudent', pathMatch:'full'},
+  {path:'**', component: PageNotFoundComponent}
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentRouteComponent,
+    StudentlistComponent,
+    PageNotFoundComponent,
+    StudentinfoComponent,
+    NamesearchPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [StudentCanDeactivateService, DepartmentService,StudentRouteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
